887df3c3b4a8105c2d9dcfede506f13e05558972
agurcikerzanvstavaj-stack <agurcikerzanvstavaj@gmail.com>
2025-10-10 16:41:21 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,28 +7,31 @@ int main() {
     int firstDigit = num / 100; // цілочисельне ділення
     cout << "Перша цифра (сотнi): " << firstDigit << endl;
 
-    // ===== Задача 2 =====
-    // Вивести число у зворотному порядку
-    cout << "\nЗадача 2: Число у зворотному порядку: ";
-    int d1 = num % 10;       // остання цифра
-    int d2 = (num / 10) % 10;// середня цифра
-    int d3 = num / 100;      // перша цифра
-    int reversed = d1 * 100 + d2 * 10 + d3;
-    cout << reversed << endl;
+ 
+    // === ЗАДАЧА 2 ===
+    cout << "=== Задача 2 ===" << endl;
+    double x;
+    cout << "Введіть x: ";
+    cin >> x;
 
-    // ===== Задача 3 =====
-    // Дано довжину паркану і довжину однієї дошки
-    int fence, board;
-    cout << "\nЗадача 3: Введiть довжину паркану: ";
-    cin >> fence;
-    cout << "Введiть довжину дошки: ";
-    cin >> board;
+    double y = cbrt(pow(x, 2) - 2 * fabs(sin(pow(x, 3))) * fabs(tan(x)) * pow(2.5, cos(x)))
+              / (0.625 * x + 2 * (log2(pow(x, 3) + 7.5)));
 
-    int boardsCount = fence / board;  // скільки дощок поміститься
-    int remainder = fence % board;    // залишок довжини
+    cout << "Результат y = " << y << endl << endl;
 
-    cout << "Кiлькiсть дошок: " << boardsCount << endl;
-    cout << "Залишок довжини: " << remainder << endl;
+    // === ЗАДАЧА 3 ===
+    cout << "=== Задача 3 ===" << endl;
+    int A, B;
+    cout << "Введіть два цілих числа A і B: ";
+    cin >> A >> B;
+
+    bool result = (A % 2 != 0) || (B % 2 != 0);
+
+    if (result)
+        cout << "Висловлювання істинне: хоча б одне число непарне." << endl;
+    else
+        cout << "Висловлювання хибне: обидва числа парні." << endl;
 
     return 0;
 }
+
